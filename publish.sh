65ce17d11a739f06e03ee17e316004eaf5d673e6
Sean Löfgren <lofgren.sean@gmail.com>
2016-01-14 18:38:42 +0000

    modify post layout, add new shelll script, and add an upcoming post draft.

--- publish.sh
+++ publish.sh
@@ -3,18 +3,22 @@
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
-hugo -t slim
+hugo
 
 # Add changes to git.
 git add -A
 
-# Commit changes.
+# Commit source changes.
 msg="rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
+
 git commit -m "$msg"
 
-# # Push source and build repos.
-# git push -f origin master
-# git subtree push --prefix=public git@github.com:seanBE/seanBE.github.io.git gh-pages
+# Move to build dir.
+cd public
+
+# Add and commit build changes.
+git add -A
+git commit -m "$msg"
